e55be70e88b500ad850ec971012e18b759689d40
Lee Stevens <projects@lee-stevens.dev>
2025-09-04 20:54:48 +0100

    Added Docker support;

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["arkane-pc", "arkane-lab"],
+  output: "standalone"
 };
 
 export default nextConfig;
