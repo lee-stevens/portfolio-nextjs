b74913112aa37319d63d839013e162bc46e9a5d4
Lee Stevens <contact@lee-stevens.dev>
2025-09-04 20:40:15 +0100

    Granted CORS for dev environments on arkane-pc and arkane-lab;

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["arkane-pc", "arkane-lab"],
 };
 
 export default nextConfig;
